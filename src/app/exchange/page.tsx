29e386fe3b7460ac412ffb7162c6cf961142d6af
Matheus Brito de Azevedo <matheus.azevedo@ourinvest.com.br>
2023-08-01 12:45:55 -0300

    fix: fix explicit return to page functions

--- src/app/exchange/page.tsx
+++ src/app/exchange/page.tsx
@@ -1,3 +1,7 @@
-export default function ExchangePage() {
+import React from 'react';
+
+const ExchangePage: React.FC = () => {
 	return <h1>Exchange!</h1>;
-}
+};
+
+export default ExchangePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import React from 'react';
+
+const HomePage: React.FC = () => {
 	return <main className="mx-20 pt-5"></main>;
-}
+};
+
+export default HomePage;
