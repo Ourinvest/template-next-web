267e5537d2c74e7b1f871fce24d20cd6e02a19b2
Matheus Brito de Azevedo <matheus.azevedo@ourinvest.com.br>
2023-08-04 13:12:58 -0300

    feat(goldends): add goldends

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import '@/styles/global.css';
+
+import { ThemeSelectorProvider, ToastProvider } from 'goldends';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -9,7 +14,13 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({
 }) => {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<ThemeSelectorProvider>
+					<ToastProvider>
+						<div>{children}</div>
+					</ToastProvider>
+				</ThemeSelectorProvider>
+			</body>
 		</html>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import { Button } from 'goldends';
+
 const HomePage: React.FC = () => {
 	return (
 		<main className="mx-20 pt-5">
 			<p>Initial Page</p>
+			<Button text="button test" />
 		</main>
 	);
 };
